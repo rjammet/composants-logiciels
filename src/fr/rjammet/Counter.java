f362531eedcf4a3fe74ec4dbc7bfc5a411b5a595
Rémy Jammet <rjammet07@gmail.com>
2020-09-20 21:33:54 +0200

    Create a class Counter

--- src/fr/rjammet/Counter.java
+++ src/fr/rjammet/Counter.java
@@ -1,46 +1,5 @@
 package fr.rjammet;
 
 public class Counter {
-
-    private final int id;
-    private final char type;
-    private int indexPrevious;
-    private int indexNew;
-
-    /**
-     * Constructor of the Counter
-     * @param id - ID of the Counter
-     * @param type - Type of the Counter
-     */
-    public Counter(int id, char type){
-        this.id = id;
-        this.type = type;
-    }
-
-    /**
-     * Updates indexes. The index on the
-     * counter is passed in parameter
-     * @param indexRelief - Index Counter
-     */
-    public void relief(int indexRelief){
-        this.indexPrevious = indexNew;
-        this.indexNew = indexRelief;
-    }
-
-    /**
-     * Calculate and return the consumption in m3
-     * for the counter
-     * @return a consumption
-     */
-    public int consumption(){
-        return indexNew - indexPrevious;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public char getType() {
-        return type;
-    }
+    
 }
